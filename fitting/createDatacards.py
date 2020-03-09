e6c100c20590d2937152ac79e225b0a84f9e9910
ekoenig4 <ekoenig4@wisc.edu>
2020-03-09 12:12:15 -0500

    pdf processes need to have rate 1

--- fitting/createDatacards.py
+++ fitting/createDatacards.py
@@ -64,8 +64,11 @@ def Add_Shape(card,proc,nuisances):
       card.addNuisance(proc,nuisance,'shape',1)
 def AddProc(card,proc,isSignal,nuisances=["JES","JER"],datadriven=['ZJets','WJets','DYJets','GJets']):
   procname = "%s_%s" % (proc,card.channel)
+  if not any( procname in hists for hists in (card.hists,card.pdfs) ): return
   if isSignal: card.addSignal(proc,shape="w:%s" % procname)
-  else:        card.addBkg(proc,shape="w:%s" % procname)
+  else:
+    rate = 1 if procname in card.pdfs else -1
+    card.addBkg(proc,shape="w:%s" % procname,rate=rate)
   
   Add_lnN(card,proc,isSignal,datadriven)
   Add_Shape(card,proc,nuisances)
@@ -73,16 +76,31 @@ def AddTF(card,tf):
   for tf in card.vars:
     card.addTransfer(tf)
 def MakeCard(ws,ch,tf=None,signal=[]):
+  print "Writing datacard_%s" % ch
   proclist = signal + ['ZJets','WJets','DYJets','GJets','DiBoson','TTJets','QCD']
   ch_card = Datacard(ch,ws)
   ch_card.hists = getHists(ws,ch)
   ch_card.vars = getVars(ws,ch,tf)
+  ch_card.pdfs = getPDFs(ws,ch)
 
   ch_card.setObservation(shape='w:data_obs_%s'%ch)
   for proc in proclist: AddProc(ch_card,proc,proc in signal)
   AddTF(ch_card,tf)
   ch_card.write()
 
+signal = ['Axial_Mchi1_Mphi1000']
+def createDatacards(wsfname,year,signal=signal):
+  input = TFile(wsfname)
+  ws = input.Get("w")
+  ws.fname = input.GetName()
+
+  MakeCard(ws,"sr_%s"%year,("WJets","wsr_to_zsr"),signal)
+  MakeCard(ws,"we_%s"%year,("WJets","we_to_sr"))
+  MakeCard(ws,"wm_%s"%year,("WJets","wm_to_sr"))
+  MakeCard(ws,"ze_%s"%year,("DYJets","ze_to_sr"))
+  MakeCard(ws,"zm_%s"%year,("DYJets","zm_to_sr"))
+  MakeCard(ws,"ga_%s"%year,("GJets","ga_to_sr"))
+  return [ "datacard_%s_%s" % (ch,year) for ch in ("sr","we","wm","ze","zm","ga")]
 if __name__ == "__main__":
   input = TFile("workspace.root")
   ws = input.Get("w")
