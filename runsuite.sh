d543a8780d768fe92a3c4d3ee343b617a9558a59
ekoenig4 <ekoenig4@wisc.edu>
2020-03-24 17:22:38 -0500

    updated scritp

--- runsuite.sh
+++ runsuite.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# ./runlimits.py -d $@ || exit 1
-# ./PlotTool/CLplotter.py -d $@ || exit 1
+./runlimits.py -d $@ && \
+    ./PlotTool/CLplotter.py -d $@ &
 ./runimpacts.py -d $@ &
 ./runCRfit.py -d $@ && \
     (python PlotTool/plotCRfit.py -d $@ & \
     ./runpulls.py -d $@) &
+
+wait
