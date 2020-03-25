17a4245523ffde82b682e798b111499a67dd411b
ekoenig4 <ekoenig4@wisc.edu>
2020-03-25 09:34:21 -0500

    remove cards

--- workspace/datacards/Mchi_1/Mphi_1000/make_workspace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ../
-text2workspace.py datacard -m 1000 -o Mphi_1000/workspace_Mphi1000.root
--- workspace/datacards/Mchi_1/make_datacard.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-combineCards.py sr_2017=../datacard_sr_2017 sr_2018=../datacard_sr_2018 we_2017=../datacard_we_2017 we_2018=../datacard_we_2018 wm_2017=../datacard_wm_2017 wm_2018=../datacard_wm_2018 ze_2017=../datacard_ze_2017 ze_2018=../datacard_ze_2018 zm_2017=../datacard_zm_2017 zm_2018=../datacard_zm_2018 ga_2017=../datacard_ga_2017 ga_2018=../datacard_ga_2018 > datacard
-sed -i 's/Mchi1/Mchi1/g' datacard
-sed -i 's/Mphi1000/Mphi$MASS/g' datacard
