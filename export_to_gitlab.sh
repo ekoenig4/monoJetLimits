c4274ec24e7c8c9c1eaea85c4204865d1ba320eb
ekoenig4 <ekoenig4@wisc.edu>
2020-06-18 09:05:26 -0500

    updated export to uw gitlab

--- export_to_gitlab.sh
+++ export_to_gitlab.sh
@@ -1,26 +1,26 @@
-gitlab="${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/monoJetLimits/cards/uw/monojet/"
+gitlab="${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/monoJetLimits/uw_mono-x_cards/monozprime/"
 
-wsall="${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/monoJetLimits/Limits/hinv_recoil/"
-ws2017="${wsall}/recoil_2017.sys/"
-ws2018="${wsall}/recoil_2018.sys/"
-wscomb="${wsall}/recoil_Run2.sys/"
+wsall="${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/monoJetLimits/Limits/ChNemPtFrac/"
+ws2017="${wsall}/ChNemPtFrac_2017.sys/"
+ws2018="${wsall}/ChNemPtFrac_2018.sys/"
+wscomb="${wsall}/ChNemPtFrac_Run2.sys/"
 
 #----- Cards -----#
 export_cards() {
     export_path=${gitlab}/cards
     mkdir -p ${export_path}
     
-    cp -v ${ws2017}/cr_fit/datacard ${export_path}/card_monojet_2017.txt
-    cp -v ${ws2017}/cr_fit/datacard.root ${export_path}/card_monojet_2017.root
+    cp -v ${ws2017}/cr_fit/datacard ${export_path}/card_monozprime_2017.txt
+    cp -v ${ws2017}/cr_fit/datacard.root ${export_path}/card_monozprime_2017.root
     cp -v ${ws2017}/cr_fit/systematics.html ${export_path}/systematics_2017.html
 
     
-    cp -v ${ws2018}/cr_fit/datacard ${export_path}/card_monojet_2018.txt
-    cp -v ${ws2018}/cr_fit/datacard.root ${export_path}/card_monojet_2018.root
+    cp -v ${ws2018}/cr_fit/datacard ${export_path}/card_monozprime_2018.txt
+    cp -v ${ws2018}/cr_fit/datacard.root ${export_path}/card_monozprime_2018.root
     cp -v ${ws2018}/cr_fit/systematics.html ${export_path}/systematics_2018.html
     
-    cp -v ${wscomb}/cr_fit/datacard ${export_path}/card_monojet_combined.txt
-    cp -v ${wscomb}/cr_fit/datacard.root ${export_path}/card_monojet_combined.root
+    cp -v ${wscomb}/cr_fit/datacard ${export_path}/card_monozprime_combined.txt
+    cp -v ${wscomb}/cr_fit/datacard.root ${export_path}/card_monozprime_combined.root
 }
 
 #----- Diagnostics -----#
@@ -28,17 +28,17 @@ export_diagnostics() {
     export_path=${gitlab}/diagnostics
     mkdir -p ${export_path}
     
-    cp -v ${ws2017}/cr_fit/fitDiagnostics_fit_CRonly_result.root ${export_path}/fitDiagnostics_monojet_2017.root
-    cp -v ${ws2017}/cr_fit/higgsCombine_fit_CRonly_result.FitDiagnostics.mH1000.root ${export_path}/higgsCombine_monojet_2017.FitDiagnostics.mH1000.root
-    cp -v ${ws2017}/cr_fit/diffNuisances_result.root ${export_path}/diffnuisances_monojet_2017.root
+    cp -v ${ws2017}/cr_fit/fitDiagnostics_fit_CRonly_result.root ${export_path}/fitDiagnostics_monozprime_2017.root
+    cp -v ${ws2017}/cr_fit/higgsCombine_fit_CRonly_result.FitDiagnostics.mH1000.root ${export_path}/higgsCombine_monozprime_2017.FitDiagnostics.mH1000.root
+    cp -v ${ws2017}/cr_fit/diffNuisances_result.root ${export_path}/diffnuisances_monozprime_2017.root
 
-    cp -v ${ws2018}/cr_fit/fitDiagnostics_fit_CRonly_result.root ${export_path}/fitDiagnostics_monojet_2018.root
-    cp -v ${ws2018}/cr_fit/higgsCombine_fit_CRonly_result.FitDiagnostics.mH1000.root ${export_path}/higgsCombine_monojet_2018.FitDiagnostics.mH1000.root
-    cp -v ${ws2018}/cr_fit/diffNuisances_result.root ${export_path}/diffnuisances_monojet_2018.root
+    cp -v ${ws2018}/cr_fit/fitDiagnostics_fit_CRonly_result.root ${export_path}/fitDiagnostics_monozprime_2018.root
+    cp -v ${ws2018}/cr_fit/higgsCombine_fit_CRonly_result.FitDiagnostics.mH1000.root ${export_path}/higgsCombine_monozprime_2018.FitDiagnostics.mH1000.root
+    cp -v ${ws2018}/cr_fit/diffNuisances_result.root ${export_path}/diffnuisances_monozprime_2018.root
     
-    cp -v ${wscomb}/cr_fit/fitDiagnostics_fit_CRonly_result.root ${export_path}/fitDiagnostics_monojet_combined.root
-    cp -v ${wscomb}/cr_fit/higgsCombine_fit_CRonly_result.FitDiagnostics.mH1000.root ${export_path}/higgsCombine_monojet_combined.FitDiagnostics.mH1000.root
-    cp -v ${wscomb}/cr_fit/diffNuisances_result.root ${export_path}/diffnuisances_monojet_combined.root
+    cp -v ${wscomb}/cr_fit/fitDiagnostics_fit_CRonly_result.root ${export_path}/fitDiagnostics_monozprime_combined.root
+    cp -v ${wscomb}/cr_fit/higgsCombine_fit_CRonly_result.FitDiagnostics.mH1000.root ${export_path}/higgsCombine_monozprime_combined.FitDiagnostics.mH1000.root
+    cp -v ${wscomb}/cr_fit/diffNuisances_result.root ${export_path}/diffnuisances_monozprime_combined.root
 }
 
 #----- Impacts -----#
@@ -46,11 +46,11 @@ export_impacts() {
     export_path=${gitlab}/impacts
     mkdir -p ${export_path}
     
-    cp -v ${ws2017}/impacts/impacts.pdf ${export_path}/impacts_monojet_2017.pdf
+    cp -v ${ws2017}/impacts/impacts.pdf ${export_path}/impacts_monozprime_2017.pdf
     
-    cp -v ${ws2018}/impacts/impacts.pdf ${export_path}/impacts_monojet_2018.pdf
+    cp -v ${ws2018}/impacts/impacts.pdf ${export_path}/impacts_monozprime_2018.pdf
     
-    cp -v ${wscomb}/impacts/impacts.pdf ${export_path}/impacts_monojet_combined.pdf
+    cp -v ${wscomb}/impacts/impacts.pdf ${export_path}/impacts_monozprime_combined.pdf
 }
 
 #----- Input -----#
@@ -58,7 +58,7 @@ export_input() {
     export_path=${gitlab}/input
     mkdir -p ${export_path}
     
-    cp -v Systematics/monojet_recoil.sys.root ${export_path}/
+    cp -v Systematics/monozprime_ChNemPtFrac.sys.root ${export_path}/
 }
 
 export_cards
